f0a2d80387d595cf022cdb719997f25e5161fc20
Zackky Muhammad <m.zackky@gmail.com>
2020-09-15 19:08:36 +0700

    Print message when success

--- setup-helmfile/src/main.ts
+++ setup-helmfile/src/main.ts
@@ -1,10 +1,11 @@
 import * as core from '@actions/core'
-import {installHelmPlugins, installHelmfile} from './setup'
+import { installHelmPlugins, installHelmfile } from './setup'
 
 async function run(): Promise<void> {
   try {
     installHelmPlugins()
     installHelmfile(core.getInput('version'))
+    core.info(`Helmfile installed`)
   } catch (error) {
     core.setFailed(error.message)
   }
