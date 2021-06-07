028815ee37b9cefb8470a3bbab78675e567d15b0
Alvin Rizki <alvin.rizki@kitabisa.com>
2021-06-07 12:11:33 +0700

    add rancher project creation

---
+++ rancher-ns-mover/src/namespace.ts
@@ -0,0 +1,18 @@
+export async function isNamespaceExist(clusterID: string, projectID: string): Promise<void> {
+  try {
+  } catch (error) {
+    throw error
+  }
+}
+
+export async function moveNamespace(
+  oldClusterID: string,
+  oldProjectID: string,
+  newClusterID: string,
+  newProjectID: string
+): Promise<void> {
+  try {
+  } catch (error) {
+    throw error
+  }
+}
